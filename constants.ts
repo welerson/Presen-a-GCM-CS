0da37c3863a44b363ae4f2ac8a81bbd9f928bec5
Welerson Faria <welersonfaril@gmail.com>
2025-11-14 12:19:08 -0300

    27 COMMIT - ADD CS FELICIDADE II

--- constants.ts
+++ constants.ts
@@ -15,7 +15,7 @@ export const INSPECTORATES: Inspectorate[] = [
 export const GUARD_RANKS: string[] = ['GCMIII', 'GCMII', 'GCMI', 'GCDII', 'GCDI'];
 
 export const MACROS = {
-  'MACRO1': { name: 'MACRO 1 (Venda Nova - Pampulha - Norte)', password: 'GCMBH@123', count: 50 },
+  'MACRO1': { name: 'MACRO 1 (Venda Nova - Pampulha - Norte)', password: 'GCMBH@123', count: 49 },
   'MACRO2': { name: 'MACRO 2 (Oeste - Noroeste - Barreiro)', password: 'BH@CS', count: 49 },
   'MACRO3': { name: 'MACRO 3 (Centro Sul - Leste - Nordeste)', password: 'CS@GCM', count: 48 },
 };
@@ -62,7 +62,7 @@ const NORTE_CENTERS = [
   'CS PRIMEIRO DE MAIO',
   'CS PROVIDÊNCIA',
   'CS CAMPO ALEGRE',
-  'CS ETELVINA CARNEiro',
+  'CS ETELVINA CARNEIRO',
   'CS ZILAH SPOSITO',
   'CS SÃO TOMAZ',
   'CS FLORAMAR',
